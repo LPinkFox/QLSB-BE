874662f3d41deb5fdd8cf455c5c2e26ce51627cb
Duc Chung <Chungcgcg@gmail.com>
2023-12-26 23:38:45 +0700

    Feat: add Payment

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { NavLink } from "react-router-dom"
-import { ShoppingCart, User } from "phosphor-react"
+import { ShoppingCart, User, UserGear, SignOut } from "phosphor-react"
 import { UserContext } from "../contexts/UserContext"
 import "./Navbar.css"
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const { user } = useContext(UserContext);
     return (
         <nav>
@@ -21,10 +22,33 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink>
-                        <User size={32} weight="thin" />
-                    </NavLink>
+                    <User className='my-user-icon' size={32} weight="thin" onClick={() => { setOpen(!open) }} />
                 </li>
+                <div className={`my-sub-menu-wrap ${open ? 'active' : 'inactive'}`}>
+                    <div className="my-sub-menu">
+                        <div className="my-user-info">
+                            <h3>{user.hoTen}<br /><span>user</span></h3>
+                        </div>
+                        <hr />
+                        <div className="my-sub-menu-link">
+                            <NavLink className={'lp'}>
+                                <UserGear size={32} weight="thin" />
+                                <h3>Thông tin người dùng</h3>
+                            </NavLink>
+                            <span></span>
+                        </div>
+                        <div className="my-sub-menu-link">
+                            <NavLink className={'lp'}>
+                                <SignOut size={32} weight="thin" />
+                                <h3>Đăng xuất</h3>
+                            </NavLink>
+                            <span></span>
+                        </div>
+
+                    </div>
+
+                </div>
+
             </ul>
         </nav>
 
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,17 +2,70 @@ import React, { useContext } from 'react';
 import Navbar from '../components/Navbar';
 import Products from '../Products';
 import { ShopContext } from '../contexts/ShopContext';
+import { UserContext } from '../contexts/UserContext';
 import { RentContext } from '../contexts/RentContext';
 import { CartItem } from './CartItem';
 import { YardItem } from './YardItem';
 import "./cart.css"; // Updated CSS file name
-
+const formatDate = (dateString) => {
+    const [month, day, year] = dateString.split('/');
+    const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+    return isNaN(new Date(formattedDate).getTime()) ? "Invalid Date" : formattedDate;
+};
 const Cart = () => {
     const { cartItems, getTotalCartAmount } = useContext(ShopContext);
-    const { rentedYard, setRentedYard, getTotalAmountYard } = useContext(RentContext);
+    const { rentedYard, getTotalAmountYard } = useContext(RentContext);
+    const { user } = useContext(UserContext);
+    console.log('cart');
+    console.log(cartItems);
+    const dataToSend = {
+        donHangSanPham: Object.keys(cartItems)
+            .filter(id => cartItems[id] > 0) // Only include products with quantity > 0
+            .map(id => ({
+                id: parseInt(id),
+                soLuongMua: cartItems[id],
+            })),
+        donHangSanBong: rentedYard.map(yard => ({
+            id: yard.id,
+            kip: yard.kip,
+            ngay: formatDate(yard.date),
+        })),
+    };
+    console.log('Data to send:', JSON.stringify(dataToSend, null, 2));
     const handlepay = () => {
+        const userId = user.id;
 
-    }
+        fetch(`http://localhost:8080/api/nguoidung/donhang/${userId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dataToSend),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.text(); // Handle the response as text
+            })
+            .then(responseText => {
+                console.log('Response from server:', responseText);
+                // Check if the responseText contains the success message
+                if (responseText === 'Nhập dữ liệu thành công') {
+
+                    // Handle success here, e.g., show a success message to the user
+                    alert('Đơn hàng đã được gửi thành công!');
+                } else {
+                    // Handle unexpected response
+                    console.error('Unexpected response:', responseText);
+                }
+            })
+            .catch(error => {
+                console.error('Lỗi khi gửi đơn hàng:', error);
+                // Handle errors
+                alert('Lỗi');
+            });
+    };
     return (
         <>
             <Navbar />
