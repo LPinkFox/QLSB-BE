8381797920b834b61b94ecea20c5c3acc7bfec0e
Duc Chung <Chungcgcg@gmail.com>
2023-12-28 23:03:32 +0700

    rm: fe-qlsb

--- fe-qlsb/src/context/user-context.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useState } from 'react'
-export const userContext = createContext(null);
-export const userContextProvider = (props) => {
-    const [userData, setUserData] = useState({});
-    const setCurUser = (userData) => {
-        setUserData(userData);
-    }
-    <userContextProvider>props.children</userContextProvider>
-}
