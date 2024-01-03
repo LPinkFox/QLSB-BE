91327e6914a49f5d5cfbeebc2a022abe2585b574
namdoan120903 <doannambka@gmail.com>
2024-01-03 22:52:38 +0700

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Navbar from "../components/Navbar"
+import Navbar from '../components/Navbar'
+
 const HomePage = () => {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    )
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
 }
 
 export default HomePage
