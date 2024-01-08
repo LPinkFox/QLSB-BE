80e525d320e4b48783775c9e90c46ba5fccddecd
Duc Chung <Chungcgcg@gmail.com>
2024-01-08 19:56:12 +0700

    Feat: add forgot password and comfirm passowrd

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -3,6 +3,7 @@ import './LoginSignup.css'
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext'
 import { logInAPI, signUpAPI } from '../API'
+import axios from 'axios';
 const LoginSignup = () => {
     const [submit, setSubmit] = useState('Login');
     const [phoneNumber, setPhoneNumber] = useState('');
@@ -11,22 +12,35 @@ const LoginSignup = () => {
     const [adress, setAdress] = useState('');
     const navigate = useNavigate();
     const { login } = useContext(UserContext);
-    
+    const [confirmPassword, setConfirmPassword] = useState('');
+
     const handerPhoneNumberChange = (event) => {
         setPhoneNumber(event.target.value);
     }
+
     const handerPassWordChange = (event) => {
         setPassword(event.target.value);
 
     }
+
     const handerNameChange = (event) => {
         setName(event.target.value);
 
     }
+
     const handerAdressChange = (event) => {
         setAdress(event.target.value);
     }
 
+    const handleConfirmPasswordChange = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    const handerForgot = () => {
+        if (submit === 'Forgot') {
+            alert('Chức năng đang được phát triển');
+        } 
+    }
     const handerLogin = async () => {
         if (submit === 'Login') {
             try {
@@ -41,13 +55,13 @@ const LoginSignup = () => {
                     },
                     body: JSON.stringify(data),
                 });
-            
+
                 if (!response.ok) {
                     throw new Error('HTTP error ' + response.status);
                 }
-            
+
                 const responseData = await response.json();
-            
+
                 // Xử lý khi request thành công
                 login(responseData);
                 alert(`Xin chào ${responseData.hoTen}`);
@@ -70,6 +84,10 @@ const LoginSignup = () => {
 
     const handerSignup = async () => {
         if (submit === 'Signup') {
+            if (password !== confirmPassword) {
+                alert('Passwords do not match');
+                return;
+            }
             try {
                 const data = {
                     "sdt": phoneNumber,
@@ -99,6 +117,23 @@ const LoginSignup = () => {
             }
         }
     }
+    const handerForgotPassword = async () => {
+        if (submit === 'Forgot password') {
+            if (password !== confirmPassword) {
+                alert('Passwords do not match');
+                return;
+            }
+            try {
+                const url = `http://localhost:8080/api/nguoidung/thaydoimatkhau?sdt=${phoneNumber}&matkhauMoi=${password}`;
+                const response = await axios.put(url);
+                setPassword('');
+                setPhoneNumber('');
+                alert('Thay đổi mật khẩu thành công');
+            } catch (error) {
+                alert('Lỗi server');
+            }
+        }
+    }
 
 
     return (
@@ -109,29 +144,67 @@ const LoginSignup = () => {
             </div>
             <div className="my-inputs">
                 <div className="my-input">
-                    <input placeholder='Số điện thoại' type="text" onChange={handerPhoneNumberChange} />
+                    <input placeholder='Số điện thoại' type="text" onChange={handerPhoneNumberChange} value={phoneNumber} />
                 </div>
                 {
                     submit === "Signup" ?
+                    <>
                         <div className="my-input">
                             <input placeholder='Họ và Tên' type="text" onChange={handerNameChange} />
                         </div>
-                        : <></>
+                        <div className="my-input">
+                            <input placeholder='Địa chỉ' type="text" onChange={handerAdressChange} />
+                        </div>
+                    </> : <></>
+                }
+                {   
+                    submit !== "Forgot password" ? 
+                    <> 
+                        <div className="my-input">
+                            <input placeholder='Password' type="password" onChange={handerPassWordChange} />
+                        </div> 
+                    </>: <></>
                 }
                 {
                     submit === "Signup" ?
                         <div className="my-input">
-                            <input placeholder='Địa chỉ' type="text" onChange={handerAdressChange} />
+                            <input placeholder='Confirm Password' type="password" onChange={handleConfirmPasswordChange} />
                         </div>
                         : <></>
                 }
-                <div className="my-input">
-                    <input placeholder='Password' type="password" onChange={handerPassWordChange} />
-                </div>
+                {
+                    submit === "Forgot password" ? 
+                    <> 
+                        <div className="my-input">
+                            <input placeholder='New passowrd' type="password" onChange={handerPassWordChange} />
+                        </div>
+                        <div className="my-input">
+                            <input placeholder='Confirm new password' type="password" onChange={handleConfirmPasswordChange} />
+                        </div>
+                    </>: <></>
+                }
             </div>
+            {   
+                submit !== "Forgot password" ?
+                <div className="my-forgot-password" onClick={() => {setSubmit('Forgot password')}}>Forgot Password? </div>
+                :
+                <div className="my-forgot-password" onClick={() => {setSubmit('Login')}}>Haved Account? </div>
+                
+            }
             <div className="my-submit-container">
-                <div className={submit === "Login" ? "my-submit my-gray" : "my-submit"} onClick={() => { setSubmit('Signup'); handerSignup(); }}>Sign Up</div>
-                <div className={submit === "Signup" ? "my-submit my-gray" : "my-submit"} onClick={() => { setSubmit('Login'); handerLogin(); }} > Login</div>
+                {
+                    submit !== "Forgot password" ?
+                    <>
+                        <div className={submit === "Login" ? "my-submit my-gray" : "my-submit"} onClick={() => { setSubmit('Signup'); handerSignup(); }}>Sign Up</div>
+                        <div className={submit === "Signup" ? "my-submit my-gray" : "my-submit"} onClick={() => { setSubmit('Login'); handerLogin(); }} > Login</div>
+                    </>
+                    : 
+                    <>
+                        <div className={submit === "Forgot password" ? "my-submit my-gray" : "my-submit"} onClick={() => {handerForgotPassword(); setSubmit('Login');} } >Sent</div>
+                    </>
+                     
+                }
+                
             </div>
         </div>
     )
